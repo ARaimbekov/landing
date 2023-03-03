75f15f50cc08b59f4a7ed709694cd995ee8c42f1
Azim <araimbekov@intant.ru>
2023-03-03 05:11:25 +0000

    little changes with logo

--- intants_website/settings.py
+++ intants_website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#d9)@!+uu*04w4p3lq=+i5@syet2bc^cndf&hwy0*92b(unt*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
